385d8a7de66c25e327bd1b50105c1f9f619bd453
Dhanaraja Kasinathan <dhanaraja@virtualpowersystems.com>
2018-04-30 01:27:02 -0700

    Modified title

--- explore.R
+++ explore.R
@@ -18,9 +18,9 @@ t10hotelclusters <- train %>% filter(hotel_cluster %in% unlist(top10hotelcluster
 
 # Plotting histogram of hotel clusters versus Posa Continent against bookings
 g = ggplot(data = t10hotelclusters, aes(x = factor(hotel_cluster), fill = factor(posa_continent))) + geom_bar()
-g + facet_wrap(~is_booking, scales = "free") + 
+g + facet_wrap(~is_booking, scales = "free", labeller = label_both) + 
   theme_classic() + 
-  labs(y = "Count", x = "Top 10 hotel cluster Ids", title = "Distribution of Booking vs POSA Continent")
+  labs(y = "Count", x = "Top 10 hotel cluster Ids", title = "Distribution of top 10 hotel clusters with respect to booking and POSA Continent")
 
 t10dest <- train %>%   
   group_by(srch_destination_id) %>% 
@@ -31,9 +31,9 @@ t10dest <- train %>%
 # Extracting the datasets to top 10 search destinations that had hits
 t10data <- train %>% filter(srch_destination_id %in% unlist(t10dest))
 
-# Plotting histogram of hotel clusters versus Posa Continent against bookings
+# Plotting histogram of top 10 searched destinations versus Posa Continent against bookings
 h = ggplot(data = t10data, aes(x = factor(srch_destination_id), fill = factor(posa_continent))) + geom_bar()
-h + facet_wrap(~is_booking, scales = "free") + 
+h + facet_wrap(~is_booking, scales = "free", labeller = label_both) + 
   theme_classic() + 
-  labs(y = "Count", x = "Top 10 search destination Ids", title = "Distribution of Booking vs POSA Continent")
+  labs(y = "Count", x = "Top 10 search destination Ids", title = "Distribution of top 10 search destinations with respect to booking and POSA Continent")
 
